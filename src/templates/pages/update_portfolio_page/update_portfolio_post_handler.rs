e9e1e44f19ba2830c12b090fc69c8224e3163e5e
seg4lt <12027886+seg4lt@users.noreply.github.com>
2024-05-12 15:05:51 -0500

    Add and update works

--- src/templates/pages/update_portfolio_page/update_portfolio_post_handler.rs
+++ src/templates/pages/update_portfolio_page/update_portfolio_post_handler.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use axum::{
     extract::State,
     http::{HeaderMap, StatusCode},
@@ -5,10 +7,15 @@ use axum::{
     Extension, Form,
 };
 use axum_extra::extract::WithRejection;
+use chrono::NaiveDate;
 use serde::Deserialize;
 use shtml::{html, Component, Render};
+use tracing_subscriber::field::debug;
 
-use crate::{auth::cookies_and_jwt::LoggedInUser, errors::api_error::ApiError};
+use crate::{
+    auth::cookies_and_jwt::LoggedInUser, errors::api_error::ApiError,
+    repository::experience::Experience, utils::internal_error,
+};
 
 #[derive(Deserialize, Debug)]
 pub struct ExperienceInput {
@@ -23,37 +30,70 @@ pub struct ExperienceInput {
 }
 
 pub async fn update_portfolio_post_handler(
-    State(_pool): State<sqlx::PgPool>,
-    WithRejection(Extension(_user), _): WithRejection<Extension<LoggedInUser>, ApiError>,
+    State(pool): State<sqlx::PgPool>,
+    WithRejection(Extension(user), _): WithRejection<Extension<LoggedInUser>, ApiError>,
     Form(experience): Form<ExperienceInput>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
     tracing::debug!("experience: {:?}", experience);
     validate_update_portfolio_post_handler(&experience)?;
-    // let error_message = "username or password is invalid".to_string();
-    // let db_value = sqlx::query!(
-    //     "SELECT id, username, password FROM rs_portfolio_user WHERE username = $1",
-    //     login.login_username
-    // )
-    // .fetch_optional(&pool)
-    // .await
-    // .map_err(bad_request)?
-    // .ok_or_else(|| anyhow::anyhow!(error_message.clone()))
-    // .map_err(|e| bad_request(&*e))?;
 
-    // if !verify_password(&db_value.password, &login.login_password).map_err(anyhow_400)? {
-    //     return Err((StatusCode::BAD_REQUEST, error_message.clone()));
-    // }
-
-    // let token = create_token(db_value.id, &db_value.username).map_err(anyhow_500)?;
+    match experience.id.filter(|id| *id > 0) {
+        Some(id) => {
+            tracing::debug!("Upadting existing experience");
+            sqlx::query!(
+                r#"
+                UPDATE rs_portfolio_experience
+                SET title = $1, company = $2, location = $3, start_date = $4, end_date = $5, description = $6
+                WHERE id = $7 and user_id = $8
+                "#,
+                experience.title,
+                experience.company,
+                experience.location,
+                experience.start_date,
+                experience
+                    .end_date
+                    .filter(|date| date.trim() != "")
+                    .map(|date| NaiveDate::from_str(&date).unwrap()),
+                experience.description,
+                id,
+                user.user_id
+            )
+            .execute(&pool)
+            .await
+            .map_err(internal_error)?;
+        }
+        None => {
+            tracing::debug!("Inserting new experience");
+            sqlx::query!(
+                r#"
+                INSERT INTO rs_portfolio_experience (title, company, location, start_date, end_date, description, user_id)
+                VALUES ($1, $2, $3, $4, $5, $6, $7)
+                "#,
+                experience.title,
+                experience.company,
+                experience.location,
+                experience.start_date,
+                experience
+                    .end_date
+                    .filter(|date| date.trim() != "")
+                    .map(|date| NaiveDate::from_str(&date).unwrap()),
+                experience.description,
+                user.user_id
+            )
+            .execute(&pool)
+            .await
+            .map_err(internal_error)?;
+        }
+    };
     let mut headers = HeaderMap::new();
-    headers.insert("hx-redirect", "/".parse().unwrap());
+    headers.insert("hx-redirect", "/update-portfolio".parse().unwrap());
     Ok((StatusCode::OK, headers, Html("".to_string())))
 }
 fn validate_update_portfolio_post_handler(
     experience: &ExperienceInput,
 ) -> Result<(), (StatusCode, String)> {
     let mut error_html = vec![];
-    if experience.title.trim() == "asdf" {
+    if experience.title.trim() == "" {
         error_html.push(html! {
             <div class="text-red-400">
             Title cannot be blank
@@ -87,6 +127,20 @@ fn validate_update_portfolio_post_handler(
             Description cannot be blank
             </div>
         });
+    }
+    if let Some(end_date) = &experience.end_date {
+        if end_date.trim() != "" {
+            match NaiveDate::from_str(end_date) {
+                Ok(_) => {}
+                Err(_) => {
+                    error_html.push(html! {
+                        <div class="text-red-400">
+                        End date is not a valid date
+                        </div>
+                    });
+                }
+            }
+        }
     }
     if error_html.len() > 0 {
         let error_html = html! { { error_html } }.to_string();
